71c74c0d70560b220607ab6ec94b9a15dc7209df
MediaNik5 <stafilopok@mail.ru>
2022-04-06 10:30:57 +0300

    fix bugs

--- TScript/tablepage/main/Table.ts
+++ TScript/tablepage/main/Table.ts
@@ -24,10 +24,10 @@ export class Table {
 
     constructor(private _store) {
         this.tableTopic = new TableTopic(parseInt(getParam('id')));
-        this.websocket.connect(this.tableTopic, message => {
+        this.websocket.subscribe(this.tableTopic, message => {
             console.log(message);
             this.cells[message.x][message.y].text = message.text;
-        });
+        })
         this.http.proceedRequest(
           new UserInfoRequest({}),
         ).then(user => {
--- TScript/util/WebSocketClient.ts
+++ TScript/util/WebSocketClient.ts
@@ -10,6 +10,7 @@ export class WebSocketClient{
     constructor(apiLink: string) {
         this.socket = new SockJS(apiLink);
         this.stompClient = Stomp.over(this.socket)
+        this.stompClient.connect()
     }
 
     public connect<In, Out>(topic: Topic<In, Out>, onMessage: (In) => unknown){
