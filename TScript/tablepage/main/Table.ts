8dbdd5eacfea6fe0f484945c206896f4fcfa5696
VladimirMynka <vladimirmynka@yandex.ru>
2022-04-09 18:28:01 +0700

    Colors for participants

--- TScript/tablepage/main/Table.ts
+++ TScript/tablepage/main/Table.ts
@@ -29,7 +29,8 @@ export class Table {
     constructor(private _store) {
         this.tableTopic = new TableTopic(parseInt(getParam('id')));
         this.websocket.subscribe(this.tableTopic, message => {
-            this.cells[message.x][message.y].addMessage(message.text, message.senderId);
+            if (message.senderId !== localStorage.getItem("userId"))
+                this.cells[message.x][message.y].addMessage(message.text, message.senderId);
         })
         this.http.proceedRequest(
           new UserInfoRequest({}),
