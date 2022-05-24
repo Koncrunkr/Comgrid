d1f2236645a7b186be9d35737bd59eb8b90074f2
MediaNik5 <stafilopok@mail.ru>
2022-05-25 00:17:46 +0300

    Smooth scrolling

--- TScript/headerScript.ts
+++ TScript/headerScript.ts
@@ -7,6 +7,26 @@ let info = {
     userId: ''
 }
 
+$(document).on('click', 'a[href^="#"]', function(e) {
+    // target element id
+    var id = $(this).attr('href');
+
+    // target element
+    var $id = $(id);
+    if ($id.length === 0) {
+        return;
+    }
+
+    // prevent standard hash navigation (avoid blinking in IE)
+    e.preventDefault();
+
+    // top position relative to the document
+    var pos = $id.offset().top;
+
+    // animated top scrolling
+    $('body, html').animate({scrollTop: pos});
+});
+
 window.onload = () => {
     const signInVk = document.getElementById('sign-in-vk');
     signInVk.onclick = () => {
